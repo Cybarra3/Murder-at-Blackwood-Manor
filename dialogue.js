62ef7c1493338b13026ccf4b157e3caf22d558db
Cybarra3 <97254329+Cybarra3@users.noreply.github.com>
2026-07-17 14:10:18 -0700

    Update dialogue.js

--- dialogue.js
+++ dialogue.js
@@ -1,41 +1,77 @@
 const dialogues = {
 
 
-    intro: [
-
-        {
-            speaker: "Detective Elias Stone",
-
-            image: "assets/characters/detective.jpg",
-
-            text:
-            "The invitation was strange. Someone wanted me here tonight.",
-
-            choices: [
-                {
-                    text: "Enter the mansion",
-                    next: 1
-                }
-            ]
-        },
+intro:[
 
 
-        {
-            speaker: "Detective Elias Stone",
+{
 
-            image: "assets/characters/detective.jpg",
 
-            text:
-            "The massive doors close behind me. The storm rages outside. Something feels wrong about this place.",
+speaker:"Detective Elias Stone",
 
-            choices: [
-                {
-                    text: "Investigate the hallway",
-                    next: null
-                }
-            ]
-        }
 
-    ]
+image:
+"assets/characters/detective.jpg",
+
+
+text:
+"The invitation was strange. Someone wanted me here tonight.",
+
+
+choices:[
+
+
+{
+
+text:"Enter the mansion",
+
+next:1
+
+}
+
+
+]
+
+
+},
+
+
+
+
+{
+
+
+speaker:"Detective Elias Stone",
+
+
+image:
+"assets/characters/detective.jpg",
+
+
+text:
+"The doors close behind me. The storm rages outside. Something feels wrong.",
+
+
+choices:[
+
+
+{
+
+text:"Investigate the hallway",
+
+next:null
+
+}
+
+
+]
+
+
+}
+
+
+
+]
+
 
 };
