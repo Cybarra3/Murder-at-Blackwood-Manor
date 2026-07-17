6030c73a733f0ac11a056ac3815895590fdb6efa
Cybarra3 <97254329+Cybarra3@users.noreply.github.com>
2026-07-17 15:04:43 -0700

    Create puzzles.js

---
+++ puzzles.js
@@ -0,0 +1,32 @@
+const puzzles = {
+
+
+studyLock:{
+
+
+type:"key",
+
+requiredItem:"Library Key",
+
+success:
+"Unlocked Victor's Study."
+
+},
+
+
+
+victorSafe:{
+
+
+type:"combination",
+
+answer:"739",
+
+success:
+"The safe opens. Inside is a mysterious journal."
+
+}
+
+
+
+};
