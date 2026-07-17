0ee22aa5c158f8cedee99bf5ef983e806e047763
Cybarra3 <97254329+Cybarra3@users.noreply.github.com>
2026-07-17 13:56:29 -0700

    Create dialogue.js

---
+++ dialogue.js
@@ -0,0 +1,41 @@
+const dialogues = {
+
+
+    intro: [
+
+        {
+            speaker: "Detective Elias Stone",
+
+            image: "assets/characters/detective.jpg",
+
+            text:
+            "The invitation was strange. Someone wanted me here tonight.",
+
+            choices: [
+                {
+                    text: "Enter the mansion",
+                    next: 1
+                }
+            ]
+        },
+
+
+        {
+            speaker: "Detective Elias Stone",
+
+            image: "assets/characters/detective.jpg",
+
+            text:
+            "The massive doors close behind me. The storm rages outside. Something feels wrong about this place.",
+
+            choices: [
+                {
+                    text: "Investigate the hallway",
+                    next: null
+                }
+            ]
+        }
+
+    ]
+
+};
