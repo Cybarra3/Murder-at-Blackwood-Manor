8ceded51a79c8dc077aa2048d1916f17e4e65e04
Cybarra3 <97254329+Cybarra3@users.noreply.github.com>
2026-07-17 13:39:52 -0700

    Create scenes.js

---
+++ scenes.js
@@ -0,0 +1,33 @@
+const scenes = {
+
+
+intro:{
+
+
+title:
+"Blackwood Manor",
+
+
+text:
+"The rain falls heavily. You arrive at the mansion after receiving a mysterious invitation."
+
+
+},
+
+
+
+study:{
+
+
+title:
+"Victor's Study",
+
+
+text:
+"The body has been discovered. The investigation begins."
+
+
+}
+
+
+};
