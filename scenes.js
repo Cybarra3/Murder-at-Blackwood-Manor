625ef6d99ac73e14edc4bb8138727450c1907173
Cybarra3 <97254329+Cybarra3@users.noreply.github.com>
2026-07-17 14:09:26 -0700

    Update scenes.js

--- scenes.js
+++ scenes.js
@@ -4,30 +4,14 @@ const scenes = {
 intro:{
 
 
-title:
-"Blackwood Manor",
+title:"Blackwood Manor",
 
 
 text:
-"The rain falls heavily. You arrive at the mansion after receiving a mysterious invitation."
-
-
-},
-
-
-
-study:{
-
-
-title:
-"Victor's Study",
-
-
-text:
-"The body has been discovered. The investigation begins."
-
+"The storm approaches. A mysterious invitation has brought you here."
 
 }
+
 
 
 };
